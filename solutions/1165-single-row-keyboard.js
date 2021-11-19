9ca20d294cbedacab53af5963413f6efbce61221
Pasha Grekovich <pgrekovich@gmail.com>
2021-11-19 19:06:51 +0100

    add alternative solution for 1165n

--- solutions/1165-single-row-keyboard.js
+++ solutions/1165-single-row-keyboard.js
@@ -4,13 +4,34 @@
  * @return {number}
  */
 
-var calculateTime = function(keyboard, word) {
+ var calculateTimeV1 = function(keyboard, word) {
     let steps = 0
     let lastIndex = 0
 
     for(i = 0; i < word.length; i++) {
         const idx = keyboard.indexOf(word[i])
         steps += Math.abs(lastIndex - idx)
+        lastIndex = idx
+    } 
+
+    return steps
+};
+
+var calculateTimeV2 = function(keyboard, word) {
+    let steps = 0
+    let lastIndex = 0
+
+    const keyboardCache= new Map()
+
+    for(i = 0; i < keyboard.length; i++) {
+        keyboardCache.set(keyboard[i], i)
+    }
+
+    for(i = 0; i < word.length; i++) {
+        const idx = keyboardCache.get(word[i])
+
+        steps += Math.abs(lastIndex - idx)
+
         lastIndex = idx
     } 
 
